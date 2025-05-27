bb0fd1a0be806f740a3055820c6af5ea4b0ffab6
Mf <47335727+Marcotty@users.noreply.github.com>
2025-05-28 00:04:11 +0200

    RecipeDetails : add elements to page

--- StockManager/Pages/RecipeDetailsPage.xaml.cs
+++ StockManager/Pages/RecipeDetailsPage.xaml.cs
@@ -4,10 +4,16 @@ namespace StockManager.Pages;
 
 public partial class RecipeDetailsPage : ContentPage
 {
-	private Recipe Recipe { get; set; }
+	public Recipe LocalRecipe { get; set; }
+
     public RecipeDetailsPage(Recipe recipe)
 	{
-        Recipe = recipe;
         InitializeComponent();
-	}
+        LocalRecipe = recipe;
+        BindingContext = this;
+    }
+    protected override void OnAppearing()
+    {
+        OnPropertyChanged(nameof(LocalRecipe));
+    }
 }
