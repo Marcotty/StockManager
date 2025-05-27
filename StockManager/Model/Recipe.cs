d3fb5aaa18e067534dc1dad7b4619bbcb0dfe711
Mf <47335727+Marcotty@users.noreply.github.com>
2025-05-28 00:03:53 +0200

    Recipe : change Image object to string url

--- StockManager/Model/Recipe.cs
+++ StockManager/Model/Recipe.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace StockManager.Model
 {
-    public class Recipe(string name, string description, List<Item> ingredients, Image image) : INotifyPropertyChanged
+    public class Recipe(string name, string description, List<Item> ingredients, string image) : INotifyPropertyChanged
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; } = name;
         public string Description { get; set; } = description;
-        public Image Image { get; set; } = image;
+        public string Image { get; set; } = image;
         public IList<Item> Ingredients { get; set; } = ingredients;
         public event PropertyChangedEventHandler? PropertyChanged;
 
