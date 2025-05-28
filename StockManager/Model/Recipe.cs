d2de24e11ada04310c9937da2386bedbf7418815
Mf <47335727+Marcotty@users.noreply.github.com>
2025-05-28 02:53:50 +0200

    Recipe : add isntructions

--- StockManager/Model/Recipe.cs
+++ StockManager/Model/Recipe.cs
@@ -7,12 +7,13 @@ using System.Threading.Tasks;
 
 namespace StockManager.Model
 {
-    public class Recipe(string name, string description, List<Item> ingredients, string image) : INotifyPropertyChanged
+    public class Recipe(string name, string description, List<Item> ingredients, string image, string instructions) : INotifyPropertyChanged
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; } = name;
         public string Description { get; set; } = description;
         public string Image { get; set; } = image;
+        public string Instructions { get; set; } = instructions;
         public IList<Item> Ingredients { get; set; } = ingredients;
         public event PropertyChangedEventHandler? PropertyChanged;
 
